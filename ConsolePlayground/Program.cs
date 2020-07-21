7e59729371d47f19507ab2ddfefd05eee3bf235c
Ben Bryant <brbryant2639@gmail.com>
2020-07-20 21:10:33 -0500

    First dealings with C#. Had a little fun on the console with the basics.

---
+++ ConsolePlayground/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace ConsolePlayground
+{
+    class Program
+    {
+        class Hobbit: IPlayer
+        {
+            public string name;
+            public Hobbit(string nm)
+            {
+                name = nm;
+            }
+            public string getName() { return name; }
+            public void Greeting()
+            {
+                Console.WriteLine("I am " + name.ToUpper() + " from the SHIRE");
+            }
+        }
+
+        class Wizard: IPlayer
+        {
+            public string name;
+            public Wizard(string nm)
+            {
+                name = nm;
+            }
+            public string getName() { return name; }
+            public void Greeting()
+            {
+                Console.WriteLine("I am " + name.ToUpper() + " and I am accompanying my dear friend.");
+            }
+        }
+
+        interface IPlayer
+        {
+            void Greeting();
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            Hobbit playerOne = new Hobbit(Console.ReadLine());
+            playerOne.Greeting();
+            Console.WriteLine("Hmm, that is a good name for a hero... Say it with pride. Be bold. Never stray from the path of wisdom and courage.");
+            Console.WriteLine("And who might you be?");
+            Wizard playerCompanion = new Wizard(Console.ReadLine());
+            playerCompanion.Greeting();
+            Console.WriteLine("Ahh, you look to be very wise. " + playerOne.getName() + " must be grateful to have your help. Let the journey BEGIN!");
+        }
+    }
+}
